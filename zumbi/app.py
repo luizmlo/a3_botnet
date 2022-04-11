ced47522fe79273965cea1c23f1e7a40c14d5081
luiz <luiz.melo@ostec.com.br>
2022-04-11 15:28:33 -0300

    inicio do front zumbi

---
+++ zumbi/app.py
@@ -0,0 +1,14 @@
+# flask app that serves the index.html file
+
+from flask import Flask, render_template
+
+name = 'zumbi'
+
+app = Flask(name)
+
+@app.route("/")
+def index():
+    return render_template('./index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4002, debug=True)
