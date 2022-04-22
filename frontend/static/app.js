a816423d1bdf763b7f6a2d34dbf77ddc6e570ed5
Luiz Melo <zberyt@gmail.com>
2022-04-21 22:00:51 -0300

    console messages funcional, criando função agora

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -1,1 +1,11 @@
 particlesJS.load('particles-js', './static/particles-config.json');
+
+// get element console-messages
+var console_messages = document.getElementById('console-messages');
+
+for (var i=0; i<=10; i++) {
+    var message = document.createElement('p')
+    message.className = 'message default';
+    message.innerText = 'message: ' + i;
+    console_messages.appendChild(message);
+}
